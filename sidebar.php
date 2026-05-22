382e82284b8d31979321396f4c1f96181701b200
Adarsh morey <moreyadarsh63@gmail.com>
2026-05-22 16:13:54 +0530

    changes done

--- sidebar.php
+++ sidebar.php
@@ -110,7 +110,7 @@ transition-all duration-300 ease-in-out overflow-y-auto shadow-2xl">
 
 <a href="student_reg_update.php"
    class="block px-4 py-2 rounded-md text-slate-400 hover:bg-slate-800 hover:text-cyan-400">
-    Update Student
+   🔽 Update Student
 </a>
 
         </div>
